d68016c74fd1cbdea56a019d6205a736074d1451
Celeste <c@eleste.me>
2024-08-20 14:50:24 -0700

    Input callbacks send samples via an async_channel
    Which should make it fairly straightforward to produces a stream of
    sample Futures that can be consumed in the UI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use async_channel;
 use cpal;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use iced::executor;
@@ -46,37 +47,59 @@ impl Application for Counter {
     }
 }
 
-fn create_input_stream() -> Box<dyn StreamTrait> {
-    let host = cpal::default_host();
-    let device = host.default_input_device().unwrap();
-    let mut supported: Option<cpal::SupportedStreamConfigRange> = None;
-    for c in device.supported_input_configs().unwrap() {
-        if c.channels() == 1 {
-            supported = Some(c);
-            break;
+struct InputStream {
+    _frames: async_channel::Receiver<Vec<u8>>,
+    _stream: Box<dyn StreamTrait>,
+}
+
+impl InputStream {
+    fn new() -> InputStream {
+        let (tx, rx) = async_channel::bounded(16);
+
+        let host = cpal::default_host();
+        let device = host.default_input_device().unwrap();
+        let mut supported: Option<cpal::SupportedStreamConfigRange> = None;
+        for c in device.supported_input_configs().unwrap() {
+            if c.channels() == 1 {
+                supported = Some(c);
+                break;
+            }
         }
+        // In theory, should check this rate is supported:
+        let config = supported.unwrap().with_sample_rate(cpal::SampleRate(44100));
+        let stream = Box::new(
+            device
+                .build_input_stream(
+                    &config.config(),
+                    move |data: &[u8], _: &cpal::InputCallbackInfo| {
+                        let len = data.len();
+                        match tx.try_send(Vec::from(data)) {
+                            Err(async_channel::TrySendError::Full(_)) => {
+                                println!("Dropped {} samples", len);
+                            },
+                            Err(async_channel::TrySendError::Closed(_)) => {
+                                println!("No receiver for {} samples", len);
+                            },
+                            Ok(()) => {
+                                println!("Input: {} samples", data.len());
+                            }
+                        }
+                    },
+                    move |err| {
+                        println!("Stream error: {}", err);
+                    },
+                    None, // blocking
+                )
+                .unwrap(),
+        );
+
+        InputStream { _frames: rx, _stream: stream }
     }
-    // In theory, should check this rate is supported:
-    let config = supported.unwrap().with_sample_rate(cpal::SampleRate(44100));
-    Box::new(
-        device
-            .build_input_stream(
-                &config.config(),
-                move |data: &[u8], _: &cpal::InputCallbackInfo| {
-                    println!("Input: {} samples", data.len())
-                },
-                move |err| {
-                    println!("Stream error: {}", err);
-                },
-                None, // blocking
-            )
-            .unwrap(),
-    )
 }
 
 fn main() -> iced::Result {
     // This will receive data in the background:
-    let _stream = create_input_stream();
+    let _stream = InputStream::new();
 
     Counter::run(Settings {
         // This is a work-around for a bug with nvidia's linux vulkan drivers,
