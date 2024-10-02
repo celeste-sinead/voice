9118ade9d106353fe31396f34d6777c188cf38e3
Celeste Sinéad <c@eleste.me>
2024-10-01 21:07:31 -0700

    Compute magnitude and unwrapped phase for FFT results

--- audio/src/dsp.rs
+++ audio/src/dsp.rs
@@ -1,5 +1,8 @@
+use std::f32::consts::PI;
+use std::iter::zip;
 use std::sync::Arc;
 
+use approx::AbsDiffEq;
 use num_complex::Complex;
 use rustfft::{Fft, FftPlanner};
 
@@ -39,11 +42,65 @@ impl FFTSequence {
         }
     }
 
-    pub fn fft(&self, period: &ChannelPeriod) -> Vec<Complex<f32>> {
+    pub fn fft(&self, period: &ChannelPeriod) -> CartesianFFT {
         let mut res: Vec<Complex<f32>> =
             period.iter().map(|y| Complex { re: *y, im: 0. }).collect();
         self.fft.process(&mut res);
-        res
+        CartesianFFT(res)
+    }
+}
+
+/// The result of a FFT, in cartesian form (re + im * i)
+#[derive(Clone, Debug, PartialEq)]
+pub struct CartesianFFT(pub Vec<Complex<f32>>);
+
+/// The result of a FFT in polar form (r * e ^ (i * Θ))
+/// i.e. magnitude + phase which is generally more useful for display
+#[derive(Clone, Debug, PartialEq)]
+pub struct PolarFFT(pub Vec<(f32, f32)>);
+
+impl CartesianFFT {
+    /// Convert to polar form, attempting to unwrap phase (i.e. remove
+    /// PI <-> -PI wrap-around discontinuities)
+    pub fn to_polar(self) -> PolarFFT {
+        PolarFFT(self.0.into_iter().map(|y| y.to_polar()).collect())
+    }
+}
+
+impl PolarFFT {
+    pub fn unwrap_phase(&mut self) {
+        let mut prev_wrapped = (0., 0.);
+        let mut prev = (0., 0.);
+        for cur in &mut self.0 {
+            // If the absolute difference betweneen the current and previous
+            // (wrapped) phases is > PI, it could be made smaller by adding
+            // or subtracting 2*PI, which is our heuristic for wrapping.
+            let mut diff = cur.1 - prev_wrapped.1;
+            if diff > PI {
+                diff -= 2. * PI;
+            } else if diff < -PI {
+                diff += 2. * PI;
+            }
+            prev_wrapped = *cur;
+            // Apply the wrap-adjusted difference to the previous unwrapped
+            // phase to get the current unwrapped phase, in order to preserve
+            // the number of rotations that's been accumulated.
+            cur.1 = prev.1 + diff;
+            prev = *cur;
+        }
+    }
+}
+
+impl AbsDiffEq for PolarFFT {
+    type Epsilon = f32;
+
+    fn default_epsilon() -> f32 {
+        f32::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &PolarFFT, epsilon: Self::Epsilon) -> bool {
+        zip(self.0.iter(), other.0.iter())
+            .all(|((r1, p1), (r2, p2))| r1.abs_diff_eq(r2, epsilon) && p1.abs_diff_eq(p2, epsilon))
     }
 }
 
@@ -72,5 +129,83 @@ mod tests {
     #[test]
     fn db_from_full_scale() {
         assert_eq!(Decibels::from_full_scale(0.1), Decibels(-10.))
+    }
+
+    #[test]
+    fn polar_unwrap_positive() {
+        let fft = CartesianFFT(vec![
+            Complex { re: -1., im: 1. },  // phase = 3/4 * PI
+            Complex { re: -1., im: -1. }, // phase wraps to -3/4 * PI
+            Complex { re: 1., im: 0. },
+            Complex { re: -1., im: 1. },
+            Complex { re: -1., im: -1. }, // another wrap
+            Complex { re: 1., im: 0. },
+            Complex { re: -1., im: 1. },
+            Complex { re: -1., im: -1. }, // another wrap
+            Complex { re: -1., im: 1. },  // and then unwrap
+        ]);
+        let mut polar = fft.to_polar();
+
+        polar.unwrap_phase();
+        let sq2 = 2f32.sqrt();
+        assert_abs_diff_eq!(
+            polar,
+            PolarFFT(vec![
+                (sq2, 0.75 * PI),
+                (sq2, 1.25 * PI),
+                (1.0, 2.00 * PI),
+                (sq2, 2.75 * PI),
+                (sq2, 3.25 * PI),
+                (1.0, 4.00 * PI),
+                (sq2, 4.75 * PI),
+                (sq2, 5.25 * PI),
+                (sq2, 4.75 * PI),
+            ]),
+            epsilon = 1e-6
+        );
+
+        // unwrap_phase should be idempotent
+        let mut polar2 = polar.clone();
+        polar2.unwrap_phase();
+        assert_eq!(polar, polar2);
+    }
+
+    #[test]
+    fn polar_unwrap_negative() {
+        let fft = CartesianFFT(vec![
+            Complex { re: -1., im: -1. }, // phase = -3/4 * PI
+            Complex { re: -1., im: 1. },  // phase wraps to 3/4 * PI
+            Complex { re: 1., im: 0. },
+            Complex { re: -1., im: -1. },
+            Complex { re: -1., im: 1. }, // another wrap
+            Complex { re: 1., im: 0. },
+            Complex { re: -1., im: -1. },
+            Complex { re: -1., im: 1. },  // another wrap
+            Complex { re: -1., im: -1. }, // and then unwrap
+        ]);
+        let mut polar = fft.to_polar();
+
+        polar.unwrap_phase();
+        let sq2 = 2f32.sqrt();
+        assert_abs_diff_eq!(
+            polar,
+            PolarFFT(vec![
+                (sq2, -0.75 * PI),
+                (sq2, -1.25 * PI),
+                (1.0, -2.00 * PI),
+                (sq2, -2.75 * PI),
+                (sq2, -3.25 * PI),
+                (1.0, -4.00 * PI),
+                (sq2, -4.75 * PI),
+                (sq2, -5.25 * PI),
+                (sq2, -4.75 * PI),
+            ]),
+            epsilon = 1e-6
+        );
+
+        // unwrap_phase should be idempotent
+        let mut polar2 = polar.clone();
+        polar2.unwrap_phase();
+        assert_eq!(polar, polar2);
     }
 }
